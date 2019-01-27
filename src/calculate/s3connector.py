7bc3130c9a629441b7eedbc3161a0ddf060ca988
zhenfengqi <dennis09121111@gmail.com>
2019-01-27 16:26:46 -0500

    Simplified S3 connector.

--- src/calculate/s3connector.py
+++ src/calculate/s3connector.py
@@ -2,18 +2,14 @@ import boto3
 
 
 class S3Connector:
-
+    """
+    Connect to AWS s3 bucket
+    """
     def __init__(self, bucket_name):
         self.s3 = boto3.resource('s3')
         self.bucket_name = bucket_name
 
-    def connect(self):
-        return self.s3.Bucket(self.bucket_name)
-
-    def get_bucket(self):
-        return self.connect()
-
     def get_objects(self):
-        bucket = self.get_bucket()
+        bucket = self.s3.Bucket(self.bucket_name)
         for object in bucket.objects.all():
             yield object
