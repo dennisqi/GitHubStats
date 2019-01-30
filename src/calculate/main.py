7aa586582ddb5462f66154ad8167a21e513e12e0
Dennis Qi <dennis09121111@gmail.com>
2019-01-30 02:20:45 +0000

    ready to spark-submit

--- src/calculate/main.py
+++ src/calculate/main.py
@@ -42,26 +42,29 @@ if __name__ == '__main__':
 
     processor.generate_processor_read_file(processor_read, processor_write, head, tail)
 
+    counter = 0
     # Read from processor_read.txt which contains lines of file names on S3
     for file_name in processor.get_lines_of_file(file=processor_read):
 
         # result_d is a dict contains date_time and num_create_events
         result_d = processor.process(bucket_name, file_name)
-        print(result_d)
-        break
-        # if not result_d:
-        #     print('RECORD IS EMPTY ' + file_name)
-        #     continue
-        #
-        # # Generate parameter and sql query to insert in to DB table
-        # insert_sql = "INSERT INTO num_repo_creation VALUES (%s, %s);"
-        # insert_param = (result_d['date_time'], result_d['num_create_events'])
-        #
-        # # Write into DB, create table only one time, then set wheather_create_table to False
-        # dbwriter.write_to_db(wheather_create_table, create_table_sql, insert_sql, insert_param, file_name)
-        # wheather_create_table = False
-        # print('INSERTED RECODR: ' + file_name)
-        #
-        # # Write the file_name into processor_write file, to keep the record
+        # print(result_d)
+        # if counter >= 10:
+        #     break
+        # counter += 1
+
+        if not result_d:
+            print('RECORD IS EMPTY ' + file_name)
+            continue
+
+        # Generate parameter and sql query to insert in to DB table
+        insert_sql = "INSERT INTO num_repo_creation_v2 VALUES (%s, %s);"
+        insert_param = (result_d['date_time'], result_d['num_create_events'])
+
+        # Write into DB, create table only one time, then set wheather_create_table to False
+        dbwriter.write_to_db(wheather_create_table, create_table_sql, insert_sql, insert_param, file_name)
+        wheather_create_table = False
+        print('INSERTED RECODR: ' + file_name)
+
+        # Write the file_name into processor_write file, to keep the record
         # processor.append_to_file(processor_write, file_name)
-        # print('WROTE RECODR: ' + file_name)
