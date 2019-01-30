9fd21fa3976dbd7e3fb58986acd79cb42dcf70ab
Dennis Qi <dennis09121111@gmail.com>
2019-01-30 18:32:21 +0000

    Modified spark processor to calculate increase rate compare to last week

--- src/calculate/spark_processor.py
+++ src/calculate/spark_processor.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 import datetime
 import subprocess
 from pyspark import SparkContext, SQLContext, SparkConf
@@ -9,29 +10,27 @@ class SparkProcessor:
 
     def __init__(self, aws_id, aws_key, host, dbname, user, password, processor_write):
         sc_conf = SparkConf()
-        sc_conf.setAppName('ghalarge_deployMode_cluster')
+        sc_conf.setAppName('ghalarge_deployMode_cluster_rate_last_week_w_dict_6G')
         sc_conf.setMaster(
             'spark://ec2-52-45-53-97.compute-1.amazonaws.com:7077')
-        # sc_conf.set('spark.executor.memory', '6g')
-        # sc_conf.set('spark.executor.cores', '6')
-        # sc_conf.set('spark.driver.cores', '6')
-        # sc_conf.set('spark.driver.memory', '6g')
-        # sc_conf.set('spark.logConf', True)
-        # sc_conf.set('spark.driver.extraClassPath', '../../lib/postgresql-42.2.5.jar')
+        sc_conf.set('spark.executor.memory', '6g')
         sc_conf.set('spark.submit.deployMode', 'cluster')
-        # sc_conf.set('spark.python.worker.memory', '6g')
-        # sc_conf.set('spark.files.maxPartitionBytes', '6g')
+        # sc_conf.set('spark.driver.extraClassPath', '../../lib/postgresql-42.2.5.jar')
+
         sc = SparkContext(conf=sc_conf)
         hadoop_conf = sc._jsc.hadoopConfiguration()
         hadoop_conf.set("fs.s3n.awsAccessKeyId", aws_id)
         hadoop_conf.set("fs.s3n.awsSecretAccessKey", aws_key)
         self.sqlContext = SQLContext(sc)
+
         self.default_start_datetime = datetime.datetime(2011, 2, 11, 0)
-        print('Connecting to DB...')
-        self.conn = psycopg2.connect(
-            host=host, database=dbname, user=user, password=password)
-        print('Connected.')
         self.processor_write = processor_write
+        self.seven_days = {}
+
+        # Connect to DB
+        print('Connecting to DB...')
+        self.conn = psycopg2.connect(host=host, database=dbname, user=user, password=password)
+        print('Connected.')
 
     def write_to_db(self, wheather_create_table, create_table_sql, insert_sql, insert_param, file_name):
         """
@@ -56,7 +55,7 @@ class SparkProcessor:
         self.conn.commit()
         cur.close()
 
-    def process(self, bucket_name, file_name):
+    def process(self, bucket_name, file_name, wheather_create_table, create_table_sql):
         """
         Given a bucket name and a file name,
             count the number of CreateEvents in the file.
@@ -84,7 +83,7 @@ class SparkProcessor:
         except Exception as e:
             self.print_error(e)
 
-        rate_last_week = self.rate_last_week(date_time, num_create_events)
+        rate_last_week = self.rate_last_week(date_time, num_create_events, wheather_create_table, create_table_sql)
 
         if num_create_events:
             return {
@@ -93,19 +92,16 @@ class SparkProcessor:
                 'rate_last_week': rate_last_week
             }
 
-    def rate_last_week(self, date_time, num_create_events):
+    def rate_last_week(self, date_time, num_create_events, wheather_create_table, create_table_sql):
         """
         Given a datetime predict and num_create_events,
             calculate the increasing rate of today compare to last week
         """
-        cur = self.conn.cursor()
-        cur.execute(
-            'SELECT num_create_events from num_repo_creation_v3 where date_time = %s',
-            (date_time - timedelta(days=7)))
-        self.conn.commit()
-        result = cur.fetch_one()
-        if not result:
+        date_time_7 = date_time - datetime.timedelta(days=7)
+        self.seven_days[date_time] = num_create_events
+        if date_time_7 not in self.seven_days:
             return 1.0
+        result = self.seven_days.pop(date_time_7)
         return 1.0 + (num_create_events - float(result)) / float(result)
 
     def get_dataframe_from_s3(self, bucket_name, file_name):
@@ -135,7 +131,7 @@ class SparkProcessor:
             date_time = datetime.datetime(
                 int(file_name_info_d['year']),
                 int(file_name_info_d['month']),
-                int(file_name_info_d['day'])
+                int(file_name_info_d['day']), 0
             )
 
         except Exception as e:
@@ -147,7 +143,7 @@ class SparkProcessor:
         """
         Given the processor_read file dir and processor_write file dir,
             generate new processor_read file that contains all file name
-            between last wrote file in processor_write and today's dateself.
+            between last wrote file in processor_write and today's date.
         """
         tail_line = self.get_head_tail_line('tail', processor_write)
         if tail_line:
@@ -190,12 +186,7 @@ class SparkProcessor:
     def parse_file_name_info_d(self, file_name):
         """
         Given a file name, return a dict contains
-        {
-            'string_before_date': 'gharchive',
-            'year': '2011',
-            'month': '02',
-            'day': '06'
-        }
+            'string_before_date', 'year', month', day'
         """
         return {
             'string_before_date': file_name[:-15],
@@ -203,7 +194,3 @@ class SparkProcessor:
             'month': file_name[-10:-8],
             'day': file_name[-7:-5]
         }
-
-    def append_to_file(self, file_append_to, data_to_append):
-        with open(file_append_to, 'a') as write_file:
-            write_file.write(data_to_append + '\n')
