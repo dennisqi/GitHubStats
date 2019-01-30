a27f6ee40149e1d9e56ffec143b7055b7e463ae1
Dennis Qi <dennis09121111@gmail.com>
2019-01-30 18:31:41 +0000

    Simplified main.py

--- src/calculate/main.py
+++ src/calculate/main.py
@@ -2,7 +2,6 @@ import os
 import time
 from datetime import timedelta
 from spark_processor import SparkProcessor
-from dbwriter import DBRwiter
 
 
 if __name__ == '__main__':
@@ -17,7 +16,6 @@ if __name__ == '__main__':
     dbname = 'dbname'
     user = os.environ.get('PG_USER')
     password = os.environ.get('PG_PSWD')
-    dbwriter = DBRwiter(host, dbname, user, password, processor_write)
 
     # SQL query for creating table
     create_table_sql = """
@@ -27,7 +25,7 @@ if __name__ == '__main__':
             increase_rate_last_week double precision NOT NULL
         );
     """
-    insert_sql = "INSERT INTO num_repo_creation_v3 VALUES (%s, %s, %d);"
+    insert_sql = "INSERT INTO num_repo_creation_v3 VALUES (%s, %s, %s);"
 
     # bucket_name is the s3 bucket that we read from
     bucket_name = 'ghalargefiletest'
@@ -37,33 +35,24 @@ if __name__ == '__main__':
 
     processor = SparkProcessor(
         os.environ.get('S3_ACCESS_KEY_ID'),
-        os.environ.get('S3_SECRET_ACCESS_KEY')
+        os.environ.get('S3_SECRET_ACCESS_KEY'),
+        host, dbname, user, password, processor_write
     )
 
     # head + datetime + tail = filename
-    # ghalarge + 2011-05-03 + .json = ghalarge2011-05-03.json
+    #   ghalarge + 2011-05-03 + .json = ghalarge2011-05-03.json
     head = 'ghalarge'
     tail = '.json'
 
-    # generate_processor_read_file_start_time = time.clock()
-    # processor.generate_processor_read_file(processor_read, processor_write, head, tail)
-    # generate_processor_read_file_end_time = time.clock()
-    # print('generate_processor_read_file takes:')
-    # print(
-    #     timedelta(
-    #         seconds=generate_processor_read_file_end_time
-    #         - generate_processor_read_file_start_time))
+    processor.generate_processor_read_file(processor_read, processor_write, head, tail)
 
     counter = 0
     # Read from processor_read.txt which contains lines of file names on S3
     for file_name in processor.get_lines_of_file(file=processor_read):
 
-        process_start_time = time.clock()
         # result_d is a dict contains date_time and num_create_events
-        result_d = processor.process(bucket_name, file_name)
-        process_end_time = time.clock()
-        print('process takes: %s' % str(counter))
-        print(timedelta(seconds=process_end_time - process_start_time))
+        result_d = processor.process(bucket_name, file_name, wheather_create_table, create_table_sql)
+        wheather_create_table = False
 
         if not result_d:
             print('RECORD IS EMPTY ' + file_name)
@@ -75,19 +64,6 @@ if __name__ == '__main__':
             result_d['num_create_events'],
             result_d['rate_last_week'])
 
-        write_to_db_start_time = time.clock()
         # Write into DB, create table only one time, then set wheather_create_table to False
         processor.write_to_db(wheather_create_table, create_table_sql, insert_sql, insert_param, file_name)
-        write_to_db_end_time = time.clock()
-        print('write_to_db takes: %s' % str(counter))
-        print(timedelta(seconds=write_to_db_end_time - write_to_db_start_time))
-        wheather_create_table = False
         print('INSERTED RECODR: ' + file_name)
-
-        # Write the file_name into processor_write file, to keep the record
-        # processor.append_to_file(processor_write, file_name)
-
-        print(result_d)
-        if counter >= 10:
-            break
-        counter += 1
