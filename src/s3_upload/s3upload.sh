4ff5b6a5357a3324f8c66f3e1f4a53558e4b70bf
Dennis Qi <dennis09121111@gmail.com>
2019-02-04 15:37:55 +0000

    Added comment for s3upload

--- src/s3_upload/s3upload.sh
+++ src/s3_upload/s3upload.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# for each line of url in url_generator_coming_urls.txt
+# wget, unzip, and send to aws S3 
+# after send to s3, store the saved url in url_generator_saved_urls.txt
 while read p; do
     gz_file=${p##*/}
     file_name=${gz_file::${#gz_file} - 3}
